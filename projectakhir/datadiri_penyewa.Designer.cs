98185e8934b5879551a5905b97374afa26a874da
febyan <febyan.putra.ft21@mail.umy.ac.id>
2023-07-10 15:52:04 +0700

    form konfirmasi pesanan

--- projectakhir/datadiri_penyewa.Designer.cs
+++ projectakhir/datadiri_penyewa.Designer.cs
@@ -180,6 +180,7 @@
             this.button1.TabIndex = 16;
             this.button1.Text = "Save";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label9
             // 
--- projectakhir/datadiri_penyewa.cs
+++ projectakhir/datadiri_penyewa.cs
@@ -26,5 +26,12 @@ namespace projectakhir
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            formkonfirmasipesanan formkonfirmasipesanan = new formkonfirmasipesanan();
+            formkonfirmasipesanan.Show();
+            this.Hide();
+        }
     }
 }
